08e3a3d1f7b26d02f3f9ff0685be426367496b3b
EATSTEAK <me@eatsteak.dev>
2022-08-01 12:49:14 +0900

    BREAKING: cleanup client and fresh new start with TailwindCSS

--- packages/client/src/lib/variables.ts
+++
@@ -1,3 +0,0 @@
-export const variables = {
-	baseUrl: import.meta.env.VITE_BASE_URL
-};
--- packages/client/svelte.config.js
+++ packages/client/svelte.config.js
@@ -1,35 +1,19 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build'
-		}),
-
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@use "src/variables.scss" as *;'
-					}
-				}
-			}
-		}
-	},
-
 	preprocess: [
 		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
+			postcss: true
 		})
-	]
+	],
+
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
