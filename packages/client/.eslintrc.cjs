6164ace1e880776b9105d5d2dc352e69c3ca2d52
EATSTEAK <me@eatsteak.dev>
2022-08-13 23:25:25 +0900

    chore: add eslint-plugin-tailwindcss

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['plugin:import/recommended', 'plugin:import/typescript', 'eslint:recommended', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['plugin:import/recommended', 'plugin:import/typescript', 'plugin:tailwindcss/recommended', 'eslint:recommended', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint', 'tailwindcss'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
