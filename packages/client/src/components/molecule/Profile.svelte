39b384a86e698bbd3d029eb731454ded3d390637
Hyomin Koo <me@eatsteak.dev>
2022-08-15 22:36:41 +0900

    feat: Profile component with dummy data

---
+++ packages/client/src/components/molecule/Profile.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Tag from "../atom/Tag.svelte";
+</script>
+
+<div class="profile">
+  <p>반갑습니다!</p>
+  <p><span class="name">하선우</span>님</p>
+  <Tag class="bg-gray-300 text-gray-700">학부 \ 글로벌미디어학부</Tag>
+  <Tag class="bg-gray-300 text-gray-700">학번 \ 20221686</Tag>
+</div>
+
+<style>
+
+    .profile {
+        @apply flex flex-col items-start gap-1;
+    }
+    p {
+        @apply font-semibold leading-5;
+    }
+    .name {
+        @apply text-5xl font-bold text-primary-800;
+    }
+</style>
+
+
