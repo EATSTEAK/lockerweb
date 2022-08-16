2da2e34872d81426c9b2989c9bb1b50e18261e18
Twince <tjsdn811@gmail.com>
2022-08-16 21:44:31 +0900

    Remove: 불필요하게 분리된 서비스 로직 삭제(기존 -> index.svelte에서 데이터 fetch) Co-authored-by: EATSTEAK <me@eatsteak.dev>

--- packages/client/src/lib/lockerStatus.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/extensions
-import { variables } from '$lib/variables';
-
-export const lockerStatus = () => {
-	fetch(variables.baseUrl + '/locker/count')
-		.then((res) => res.json())
-		.then(data);
-};
