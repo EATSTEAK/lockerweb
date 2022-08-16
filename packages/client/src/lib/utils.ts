36db2bfc447e3a49b1987e833849b205667df322
Twince <tjsdn811@gmail.com>
2022-08-16 21:55:05 +0900

    Feat: 사물함 수 표기를 위한 데이터 가공(비즈니스 로직 분기) Co-authored-by: EATSTEAK <me@eatsteak.dev>

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -1,2 +1,24 @@
 export const getCookieValue = (name: string) =>
 	document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '';
+
+export function getDepartmentLockerCountsByFloor(config: ServiceConfig, departmentId: string): { [floor: string]: number } {
+
+	function countLockers([from, to]: [number, number]): number {
+		return to - from + 1;
+	}
+	const deptLockerSubsections: [string, LockerSubsection[]][] = Object.values(config.buildings).flatMap((building) => {
+		return Object.entries(building.lockers)
+				.map<[string, LockerSubsection[]]>(([floor, sections]) => {
+					const subsections: LockerSubsection[] = Object.values(sections)
+						.flatMap<LockerSubsection>((section: LockerSection) => section.subsections
+							.filter((subsection) => subsection.department === departmentId)
+						)
+					return [floor, subsections];
+				}).filter(([floor, subsections]) => subsections.length)
+	});
+	return deptLockerSubsections.reduce<{ [floor: string]: number }>((result, [floor, subsections]) => {
+		if(typeof result[floor] !== 'number') result[floor] = 0;
+		result[floor] += subsections.reduce<number>((a, subsection) => a + countLockers(subsection.range), 0);
+		return result;
+	},{});
+};
