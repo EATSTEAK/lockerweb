810177863b435fdb5f5d76634c5eedcb1079bb89
Twince <tjsdn811@gmail.com>
2022-08-16 20:53:18 +0900

    feat: 사물함 `count`값을 각 학부에 맞게 분리하기 위한 타입 분리
    Co-authored-by: EATSTEAK <me@eatsteak.dev>

--- packages/client/src/lib/types.ts
+++ packages/client/src/lib/types.ts
@@ -1,18 +1,3 @@
 export type LockerCount = {
-	[department: string]: {
-		canRserve: boolean;
-		lockerLeft: number;
-		totalLocker: number;
-		availableFrom: Date;
-		availableTo: Date;
-		contact: string;
-		floors: {
-			[floor: string]: {
-				canReserve: boolean;
-				percentage: number;
-				totalLocker: number;
-				lockerLeft: number;
-			};
-		};
-	};
+	[department: string]: DepartmentLockerCount;
 };
