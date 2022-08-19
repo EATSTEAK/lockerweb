d8a48bb6a4121f5ebe3ce4e15e25531908a2e4ca
EATSTEAK <me@eatsteak.dev>
2022-08-20 02:01:13 +0900

    feat: load profile data from server

--- packages/client/src/components/molecule/Profile.svelte
+++ packages/client/src/components/molecule/Profile.svelte
@@ -1,22 +1,33 @@
-<script>
-  import Tag from "../atom/Tag.svelte";
-</script>
+<script lang='ts'>
+  import Tag from '../atom/Tag.svelte';
+  import { getDepartmentNameById } from '$lib/utils';
+  import { config } from '$lib/store';
 
-<div class="profile">
-  <p>반갑습니다!</p>
-  <p><span class="name">하선우</span>님</p>
-  <Tag class="bg-gray-300 text-gray-700">학부 \ 글로벌미디어학부</Tag>
-  <Tag class="bg-gray-300 text-gray-700">학번 \ 20221686</Tag>
-</div>
+  export let user: User;
+</script>
+{#if user}
+	<div class='profile'>
+		<p>반갑습니다!</p>
+		<p><span class='name'>{user?.name ?? '알 수 없음'}</span>님</p>
+		<Tag class='bg-gray-300 text-gray-700'>학부 \ {getDepartmentNameById($config, user.department)}</Tag>
+		<Tag class='bg-gray-300 text-gray-700'>학번 \ {user.id}</Tag>
+	</div>
+{:else}
+	<div class='profile'>
+		로딩중
+	</div>
+{/if}
 
 <style>
 
     .profile {
         @apply flex flex-col items-start gap-1;
     }
+
     p {
         @apply font-semibold leading-5;
     }
+
     .name {
         @apply text-5xl font-bold text-primary-800;
     }
