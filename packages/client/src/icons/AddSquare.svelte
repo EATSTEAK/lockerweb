71fbe96aa0770167e3985a7c2e292d7d266425c6
EATSTEAK <me@eatsteak.dev>
2022-08-21 04:55:02 +0900

    feat: add `Add`, `AddSquare`, `Delete`, `SaveEdit`, `SelectObject` and `Subtract` icons

---
+++ packages/client/src/icons/AddSquare.svelte
@@ -0,0 +1,40 @@
+<script lang='ts'>
+	let className = undefined;
+	export { className as class };
+	export let id = undefined;
+	export let tabindex = undefined;
+	export let focusable = false;
+	export let title = undefined;
+	export let style = undefined;
+
+	$: ariaLabel = $$props['aria-label'];
+	$: ariaLabelledBy = $$props['aria-labelledby'];
+	$: labelled = ariaLabel || ariaLabelledBy || title;
+	$: attributes = {
+		'aria-label': ariaLabel,
+		'aria-labelledby': ariaLabelledBy,
+		'aria-hidden': labelled ? undefined : true,
+		role: labelled ? 'img' : undefined,
+		focusable: tabindex === '0' ? true : focusable,
+		tabindex
+	};
+</script>
+<svg on:click
+		 on:mouseover
+		 on:mouseenter
+		 on:mouseleave
+		 on:keyup
+		 on:keydown
+		 width='24'
+		 height='24'
+		 fill='none'
+		 viewBox='0 0 24 24'
+		 xmlns='http://www.w3.org/2000/svg'
+		 class={className}
+		 {style}
+		 {id}
+		 {...attributes}>
+	<path
+		d='M3 6.25A3.25 3.25 0 0 1 6.25 3h11.5A3.25 3.25 0 0 1 21 6.25v11.5A3.25 3.25 0 0 1 17.75 21H6.25A3.25 3.25 0 0 1 3 17.75V6.25Zm9.75 1.5a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z'
+		fill='currentColor' />
+</svg>
