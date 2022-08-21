1ce7d4ac803278f4e1d5d7ea16795ca465495b86
Twince <tjsdn811@gmail.com>
2022-08-22 03:39:01 +0900

    feat: `/reserve`페이지 퍼블리싱 작업

---
+++ packages/client/src/components/molecule/reserve/LockerReserveInfo.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+import SelectionListItemGroup from "../../atom/SelectionListItemGroup.svelte";
+import SelectionListItem from "../../atom/SelectionListItem.svelte";
+import LockerItem from "./LockerItem.svelte";
+</script>
+
+<div class="wrap">
+    <div class="select-info">
+        <div class="select-location">
+            <h4 class="text-3xl my-2 ml-4">구역 선택</h4>
+            <div class="location-depths">
+                <div class="select-floor">
+                    <SelectionListItemGroup class="location-select-group">
+                        <SelectionListItem class="location-select-item"></SelectionListItem>
+                    </SelectionListItemGroup>
+                </div>
+                <div class="select-area">
+                    <SelectionListItemGroup>
+                        <SelectionListItem class="location-select-item"></SelectionListItem>
+                    </SelectionListItemGroup>
+                </div>
+            </div>
+        </div>
+        <div class="locker-map">
+            <img class="map-img" src="/floorMaps/1F.svg" alt="정보과학관 1층 이미지" aria-level="정보과학관 1층 이미지">
+        </div>
+    </div>
+    <div class="locker-grid">
+        <LockerItem />
+    </div>
+</div>
+
+<style>
+    .wrap{
+        @apply w-full h-full;
+    }
+
+    /* -------------- 영역 선택 및 지도 -------------- */
+    .select-info {
+        @apply flex flex-row h-2/5 min-h-[370px];
+    }
+    .select-location {
+        @apply bg-slate-300 w-2/5 md:min-w-[480px];
+    }
+    .locker-map {
+        @apply bg-slate-200 w-3/5;
+    }
+
+
+    .location-depths {
+        @apply w-full h-5/6 flex;
+    }
+    .select-floor{
+        @apply pl-8 pr-1 w-1/2;
+    }
+    .select-area {
+        @apply pr-8 pl-1 w-1/2;
+    }
+
+    .location-select-group{
+        @apply h-full;
+    }
+    :global(.location-select-item) {
+        @apply h-11;
+    }
+
+
+    .locker-map {
+        @apply flex;
+    }
+    .map-img {
+        @apply flex ml-auto mr-auto mt-10 h-4/5;
+    }
+    /* -------------- 사물함 그리드 영역 -------------- */
+    .locker-grid {
+        @apply grow flex flex-row flex-wrap overflow-y-auto;
+    }
+    .locker-grid::-webkit-scrollbar{
+        display:none;
+    }
+    .locker-grid {
+        -ms-overflow-style: none;  /* IE and 엣지 */
+        scrollbar-width: none;  /* 파이어폭스 */
+    }
+</style>
---
+++ packages/client/src/routes/reserve.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import ChevronDown from '../icons/ChevronDown.svelte';
+    import type { LockerCount} from "$lib/types";
+    import SelectionListItemGroup from "../components/atom/SelectionListItemGroup.svelte";
+    import SelectionListItem from "../components/atom/SelectionListItem.svelte";
+    import Profile from "../components/molecule/Profile.svelte";
+    import {user} from "$lib/store.js";
+    import Soongsil from "../icons/Soongsil.svelte";
+    import Button from "../components/atom/Button.svelte";
+    import UserReservedLocker from "../components/molecule/reserve/UserReservedLocker.svelte";
+    import Logout from "../icons/Logout.svelte";
+    import LockerRserveInfo from "../components/molecule/reserve/LockerReserveInfo.svelte";
+
+    let callbackUrl = undefined;
+    let countData: LockerCountResponse;
+    let lockerCount: LockerCount;
+
+</script>
+<div class='root'>
+    <div class='side-wrap'>
+        <div class="profile-wrap">
+            <div class='logo'>
+                <Soongsil class='w-20 h-20' />
+            </div>
+            <Profile class="profile" user={$user}/>
+            <div class="divide-line"></div>
+        </div>
+
+        <div class="user-info">
+           <UserReservedLocker reservedSection="A" reservedNumber="15" tillTime="00:00 ~ 14:00"/>
+        </div>
+        <div class="flex justify-between w-full">
+            <Button isIconRight="{false}" class="bg-primary-800 text-white text-center flex justify-between">
+                로그아웃
+            </Button>
+            <div class="contact-text">
+                <p>글로벌미디어학부</p>
+                <p>010-1234-1234</p>
+            </div>
+        </div>
+    </div>
+    <div class='locker-reserve-info-wrap'>
+        <LockerRserveInfo />
+    </div>
+</div>
+
+
+<style>
+    .root {
+        @apply flex flex-col md:flex-row items-stretch;
+    }
+    .logo {
+        @apply top-2;
+    }
+    .side-wrap {
+        @apply bg-gray-200 w-full md:min-w-[380px] md:w-[380px] flex flex-col justify-between min-h-screen px-10 pb-5 pt-10;
+    }
+    .locker-reserve-info-wrap {
+        @apply grow;
+    }
+    .profile-wrap {
+        @apply flex flex-col gap-10;
+    }
+
+    .divide-line {
+        @apply w-full h-0.5 bg-gray-300 mt-6;
+    }
+
+    .user-info {
+        @apply w-full h-96;
+    }
+</style>
