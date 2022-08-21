5b6d03db4f1aaf21fa3c1fbba71b3026c206df89
EATSTEAK <me@eatsteak.dev>
2022-08-21 21:32:35 +0900

    feat: add `ContentSettings`, `PeopleSettings`, `Settings`, `Heart`, `Svelte`, `TailwindCss` and `GitHub` icons

---
+++ packages/client/src/icons/brands/TailwindCss.svelte
@@ -0,0 +1,40 @@
+<script lang='ts'>
+	let className = undefined;
+	export { className as class };
+	export let id = undefined;
+	export let tabindex = undefined;
+	export let focusable = false;
+	export let title = undefined;
+	export let style = undefined;
+
+	$: ariaLabel = $$props['aria-label'];
+	$: ariaLabelledBy = $$props['aria-labelledby'];
+	$: labelled = ariaLabel || ariaLabelledBy || title;
+	$: attributes = {
+		'aria-label': ariaLabel,
+		'aria-labelledby': ariaLabelledBy,
+		'aria-hidden': labelled ? undefined : true,
+		role: labelled ? 'img' : undefined,
+		focusable: tabindex === '0' ? true : focusable,
+		tabindex
+	};
+</script>
+<svg on:click
+		 on:mouseover
+		 on:mouseenter
+		 on:mouseleave
+		 on:keyup
+		 on:keydown
+		 width='24'
+		 height='24'
+		 fill='none'
+		 viewBox='0 0 24 24'
+		 xmlns='http://www.w3.org/2000/svg'
+		 class={className}
+		 {style}
+		 {id}
+		 {...attributes}>
+	<path
+		d='M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z'
+		fill='currentColor' />
+</svg>
