96b2fde403e7d958ff412da8003afb311ea59d97
EATSTEAK <me@eatsteak.dev>
2022-08-21 20:23:32 +0900

    feat: make class applicable to SelectScreen

--- packages/client/src/components/atom/SelectScreen.svelte
+++ packages/client/src/components/atom/SelectScreen.svelte
@@ -1,14 +1,16 @@
 <script>
+	let clazz;
+	export { clazz as class };
 	import SelectObject from '../../icons/SelectObject.svelte';
 </script>
 
-<div class='please-select'>
+<div class={clazz}>
 	<SelectObject class='rotate-stop w-16 h-16' />
 	<p>대상을 선택하세요...</p>
 </div>
 
 <style>
-    .please-select {
+    div {
         @apply h-full bg-gray-200 text-gray-400 flex flex-col justify-center items-center;
     }
 
