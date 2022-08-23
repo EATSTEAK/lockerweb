f433a18c45ea89da0fb73e3af23545bfc27284b3
EATSTEAK <me@eatsteak.dev>
2022-08-24 04:04:44 +0900

    feat: validate SubsectionEntry and hand over to SectionSettings

--- packages/client/src/components/molecule/admin/service/SubsectionSettings.svelte
+++ packages/client/src/components/molecule/admin/service/SubsectionSettings.svelte
@@ -2,18 +2,46 @@
 	import SubsectionEntry from './SubsectionEntry.svelte';
 	import Button from '../../../atom/Button.svelte';
 	import Add from '../../../../icons/Add.svelte';
+	import { config } from '$lib/store';
+
+
+	$: departmentIds = $config ? $config.filter(v => v.id !== 'SERVICE').map(v => v.id) : [];
 
 	export let subsections: LockerSubsection[];
+
+	let newSubsections: LockerSubsection[] = [...subsections];
+
+	function addSubsection() {
+		newSubsections = [...newSubsections, { range: [0, 0], department: '' }];
+	}
+
+	function isValidSubsection(subsection: LockerSubsection) {
+		return typeof subsection.range[0] === 'number' &&
+			typeof subsection.range[1] === 'number' &&
+			subsection.range[0] <= subsection.range[1] &&
+			departmentIds.includes(subsection.department);
+	}
+
+	function removeSubsection(index: number) {
+		newSubsections = newSubsections.filter((v, idx) => idx !== index);
+		subsections = [...newSubsections.filter(isValidSubsection)];
+	}
+
+	function updateSubsection(index: number, evt: CustomEvent<LockerSubsection>) {
+		newSubsections[index] = evt.detail;
+		subsections = [...newSubsections.filter(isValidSubsection)];
+	}
 </script>
 
 <div class='wrap'>
 	<p class='text-xl font-bold'>세부 구역 설정</p>
-	{#each subsections as subsection, index}
+	{#each newSubsections as subsection, index}
 		<div class='my-1'>
-			<SubsectionEntry key={index} subsection={subsection} />
+			<SubsectionEntry on:remove={() => removeSubsection(index)} on:change={(evt) => updateSubsection(index, evt)}
+											 key={index} subsection={subsection} />
 		</div>
 	{/each}
-	<Button class='bg-white' isIconRight>
+	<Button on:click={addSubsection} class='bg-white' isIconRight>
 		세부 구역 추가
 		<Add slot='icon' />
 	</Button>
