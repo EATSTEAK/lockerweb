a54628f9d29c849e0f72287106de21af028dc5c0
EATSTEAK <me@eatsteak.dev>
2022-08-24 05:27:54 +0900

    feat: create loading and error screen in UserSettings tab

---
+++ packages/client/src/components/atom/ErrorScreen.svelte
@@ -0,0 +1,19 @@
+<script>
+	import ErrorCircle from '../../icons/ErrorCircle.svelte';
+
+	export let errorMessage = '오류가 발생하였습니다. 관리자에게 문의하십시오.';
+	let clazz;
+	export { clazz as class };
+</script>
+
+<div class={clazz}>
+	<ErrorCircle class='w-16 h-16' />
+	<h4>오류!</h4>
+	<p>{errorMessage}</p>
+</div>
+
+<style>
+    div {
+        @apply h-full bg-red-200 text-gray-700 flex flex-col justify-center items-center;
+    }
+</style>
