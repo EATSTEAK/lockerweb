c9039da43c44287a2b9ab43d3a0a6061e2372fcb
EATSTEAK <me@eatsteak.dev>
2022-08-27 23:27:59 +0900

    feat: create PageTitle component

---
+++ packages/client/src/components/atom/PageTitle.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+	import { config } from '$lib/store';
+
+	export let name: string;
+
+	$: serviceName = $config?.find((c: Config) => c.id === 'SERVICE').name ?? '사물함 시스템';
+</script>
+
+<svelte:head>
+	<title>{name ? `${name} - ` : ''}{serviceName}</title>
+</svelte:head>
