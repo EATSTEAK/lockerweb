becdbb0fb5558b987b6b015a7c0062447dcddffe
EATSTEAK <me@eatsteak.dev>
2022-08-28 00:35:39 +0900

    feat: renaming html files for accessing without extension on web

--- packages/client/scripts/deploy.js
+++ packages/client/scripts/deploy.js
@@ -28,5 +28,46 @@ exec('aws cloudformation describe-stacks --stack-name ssu-it-locker', (err, stdo
 			return;
 		}
 		console.log(`${stdout}`);
+		console.log('File uploaded, renaming html...');
+		exec(`aws s3api list-objects --bucket ${s3Name}`, (err, stdout, stderr) => {
+			if (err) {
+				console.error(`Exception thrown: ${err.message}`);
+				return;
+			}
+			if (stderr) {
+				console.error(`Error occurred: ${stderr}`);
+				return;
+			}
+			const files = JSON.parse(stdout);
+			const contents = files['Contents'];
+			contents.forEach((content) => {
+				const key = content['Key'];
+				if (
+					key.endsWith('.html') &&
+					!key.endsWith('index.html') &&
+					!key.endsWith('400.html') &&
+					!key.endsWith('403.html') &&
+					!key.endsWith('404.html')
+				) {
+					exec(
+						`aws s3 mv s3://${s3Name}/${key} s3://${s3Name}/${key.substr(
+							0,
+							key.lastIndexOf('.html')
+						)}`,
+						(err, stdout, stderr) => {
+							if (err) {
+								console.error(`Exception thrown: ${err.message}`);
+								return;
+							}
+							if (stderr) {
+								console.error(`Error occurred: ${stderr}`);
+								return;
+							}
+							console.log(`${stdout}`);
+						}
+					);
+				}
+			});
+		});
 	});
 });
