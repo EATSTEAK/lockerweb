56b1835e23d966b069faed7253ed604442f35b6e
Twince <tjsdn811@gmail.com>
2022-08-29 02:30:28 +0900

    feat: add page title on reserve page

--- packages/client/src/routes/reserve/+page.svelte
+++ packages/client/src/routes/reserve/+page.svelte
@@ -1,48 +1,46 @@
-<script lang="ts">
-  import NavigationShell from "../../components/molecule/NavigationShell.svelte";
-  import ChevronDown from "../../icons/ChevronDown.svelte";
-  import type { LockerCount } from "$lib/types";
-  import SelectionListItemGroup from "../../components/atom/SelectionListItemGroup.svelte";
-  import SelectionListItem from "../../components/atom/SelectionListItem.svelte";
-  import Profile from "../../components/molecule/Profile.svelte";
-  import { user } from "$lib/store.js";
-  import Soongsil from "../../icons/Soongsil.svelte";
-  import Button from "../../components/atom/Button.svelte";
-  import UserReservedLocker from "../../components/molecule/reserve/UserReservedLocker.svelte";
-  import LockerReserveInfo from "../../components/molecule/reserve/LockerReserveInfo.svelte";
-  import Skeleton from "../../components/atom/Skeleton.svelte";
-  import ArrowExportLtr from "../../icons/ArrowExportLtr.svelte";
+<script lang='ts'>
+	import NavigationShell from '../../components/molecule/NavigationShell.svelte';
+	import type { LockerCount } from '$lib/types';
+	import Button from '../../components/atom/Button.svelte';
+	import UserReservedLocker from '../../components/molecule/reserve/UserReservedLocker.svelte';
+	import LockerReserveInfo from '../../components/molecule/reserve/LockerReserveInfo.svelte';
+	import Skeleton from '../../components/atom/Skeleton.svelte';
+	import ArrowExportLtr from '../../icons/ArrowExportLtr.svelte';
+	import PageTitle from '../../components/atom/PageTitle.svelte';
 
-  let callbackUrl = undefined;
-  let countData: LockerCountResponse;
-  let lockerCount: LockerCount;
+	let callbackUrl = undefined;
+	let countData: LockerCountResponse;
+	let lockerCount: LockerCount;
 
-  let fetchContactStatus: boolean = false;
+	let fetchContactStatus: boolean = false;
 </script>
+
+<PageTitle name='예약하기' />
+
 <NavigationShell>
-  <div class="w-full h-96" slot="navigation_content">
-    <UserReservedLocker reservedSection="A" reservedNumber="15" tillTime="00:00 ~ 14:00" />
-  </div>
-  <div class="flex justify-between w-full" slot="navigation_footer">
-    <Button class="bg-primary-800 text-white" isIconRight={true} href="/logout">
-      <ArrowExportLtr slot="icon" />
-      로그아웃
-    </Button>
-    {#if fetchContactStatus}
-      <div class="contact-text">
-        <p>글로벌미디어학부</p>
-        <p>010-1234-1234</p>
-      </div>
-    {:else}
-      <div class="flex flex-col">
-        <Skeleton class="contact-text-skeleton min-w-2/5"></Skeleton>
-        <Skeleton class="contact-number-skeleton"></Skeleton>
-      </div>
-    {/if}
-  </div>
-  <div class="grow overflow-scroll">
-    <LockerReserveInfo />
-  </div>
+	<div class='w-full h-96' slot='navigation_content'>
+		<UserReservedLocker reservedSection='A' reservedNumber='15' tillTime='00:00 ~ 14:00' />
+	</div>
+	<div class='flex justify-between w-full' slot='navigation_footer'>
+		<Button class='bg-primary-800 text-white' isIconRight={true} href='/logout'>
+			<ArrowExportLtr slot='icon' />
+			로그아웃
+		</Button>
+		{#if fetchContactStatus}
+			<div class='contact-text'>
+				<p>글로벌미디어학부</p>
+				<p>010-1234-1234</p>
+			</div>
+		{:else}
+			<div class='flex flex-col'>
+				<Skeleton class='contact-text-skeleton min-w-2/5'></Skeleton>
+				<Skeleton class='contact-number-skeleton'></Skeleton>
+			</div>
+		{/if}
+	</div>
+	<div class='grow overflow-scroll'>
+		<LockerReserveInfo />
+	</div>
 
 </NavigationShell>
 
