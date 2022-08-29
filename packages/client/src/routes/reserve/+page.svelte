6cbf5a5f1996ed16865d02e9ae63ac8882aafb5f
Twince <tjsdn811@gmail.com>
2022-08-30 05:46:38 +0900

    feat: 예약한 사물함 데이터 표시를 위한 함수 생성 및 표기

--- packages/client/src/routes/reserve/+page.svelte
+++ packages/client/src/routes/reserve/+page.svelte
@@ -7,29 +7,61 @@
 	import Skeleton from '../../components/atom/Skeleton.svelte';
 	import ArrowExportLtr from '../../icons/ArrowExportLtr.svelte';
 	import PageTitle from '../../components/atom/PageTitle.svelte';
+	import { config, user } from '$lib/store';
 
 	let callbackUrl = undefined;
 	let countData: LockerCountResponse;
 	let lockerCount: LockerCount;
 
-	let fetchContactStatus: boolean = false;
+	let fetchStatus: boolean = false;
+
+
+	let lockerSection: string;
+	let lockerNumber: number;
+	let contact: string;
+	let contactDepartment: string;
+
+
+	$:if ($user) {
+		userReserveLockerConverter($user);
+		if ($config) {
+			loadContact($user, $config);
+		}
+	}
+
+	function userReserveLockerConverter(userInfo: User) {
+		if (userInfo?.lockerId) {
+			const [building, floors, location] = userInfo.lockerId.split('-');
+			lockerSection = location[0];
+			lockerNumber = parseInt(location.slice(1));
+		}
+	}
+
+	function loadContact(userInfo: User, config: Array<Config>) {
+		console.log(config);
+		if (userInfo?.department) {
+			const isDepartmentSame = config.find(config => config.id === userInfo.department);
+			contact = isDepartmentSame.contact;
+			contactDepartment = isDepartmentSame.name;
+		}
+	}
 </script>
 
 <PageTitle name='예약하기' />
 
 <NavigationShell>
 	<div class='w-full h-96' slot='navigation_content'>
-		<UserReservedLocker reservedSection='A' reservedNumber='15' tillTime='00:00 ~ 14:00' />
+		<UserReservedLocker reservedSection={lockerSection} reservedNumber={lockerNumber} tillTime='00:00 ~ 14:00' />
 	</div>
-	<div class='flex justify-between w-full' slot='navigation_footer'>
+	<div class='flex justify-between items-center w-full' slot='navigation_footer'>
 		<Button class='bg-primary-800 text-white' isIconRight={true} href='/logout'>
 			<ArrowExportLtr slot='icon' />
 			로그아웃
 		</Button>
-		{#if fetchContactStatus}
-			<div class='contact-text'>
-				<p>글로벌미디어학부</p>
-				<p>010-1234-1234</p>
+		{#if contact}
+			<div class='contact-text text-right'>
+				<p>{contactDepartment}</p>
+				<p>{contact}</p>
 			</div>
 		{:else}
 			<div class='flex flex-col'>
