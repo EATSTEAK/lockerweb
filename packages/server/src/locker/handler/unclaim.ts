b17e2ed25599e56501abab31e4b5bcf43536c802
Hyomin Koo <me@eatsteak.dev>
2022-09-03 14:30:06 +0900

    refactor: error handling in `locker` handlers

--- packages/server/src/locker/handler/unclaim.ts
+++ packages/server/src/locker/handler/unclaim.ts
@@ -1,49 +1,24 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda';
 import { createResponse } from '../../common';
-import type { JwtPayload } from 'jsonwebtoken';
-import * as jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../../env';
 import { unclaimLocker } from '../data';
-import { errorResponse, isResponsibleError, ResponsibleError } from '../../util/error';
+import { BlockedError, errorResponse, responseAsResponsibleError } from '../../util/error';
 import { queryConfig } from '../../config/data';
 import { adminId } from '../../util/database';
+import { getBlockedDepartments, verifyPayload } from '../../util/access';
 
 export const unclaimLockerHandler: APIGatewayProxyHandler = async (event) => {
 	const token = (event.headers.Authorization ?? '').replace('Bearer ', '');
-	let payload: JwtPayload;
 	try {
-		payload = jwt.verify(token, JWT_SECRET) as JwtPayload;
+		const payload = verifyPayload(token);
 		const id = payload.aud as string;
-		const config = await queryConfig();
-		const blockedDepartments = config
-			.filter((c) => {
-				const activateFrom = new Date(c.activateFrom);
-				const activateTo = new Date(c.activateTo);
-				return (
-					(c.activateFrom && activateFrom.getTime() > Date.now()) ||
-					(c.activateTo && activateTo.getTime() < Date.now())
-				);
-			})
-			.map((c) => c.id);
+		const configs = await queryConfig();
+		const blockedDepartments = getBlockedDepartments(configs);
 		if (adminId !== id && blockedDepartments.includes('SERVICE')) {
-			return createResponse(403, {
-				success: false,
-				error: 403,
-				errorDescription: 'Forbidden'
-			});
+			return errorResponse(new BlockedError('Service unavailable'));
 		}
 		const res = await unclaimLocker(id, token, blockedDepartments);
 		return createResponse(200, { success: true, result: res });
 	} catch (e) {
-		if (!isResponsibleError(e)) {
-			console.error(e);
-			const res = {
-				success: false,
-				error: 500,
-				errorDescription: 'Internal error'
-			};
-			return createResponse(500, res);
-		}
-		return errorResponse(e as ResponsibleError);
+		responseAsResponsibleError(e);
 	}
 };
