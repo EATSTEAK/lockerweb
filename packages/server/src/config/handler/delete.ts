23b1b57534be3e934db17dbac86c2f72198272d1
Hyomin Koo <me@eatsteak.dev>
2022-09-03 14:29:56 +0900

    refactor: error handling in `config` handlers

--- packages/server/src/config/handler/delete.ts
+++ packages/server/src/config/handler/delete.ts
@@ -1,11 +1,9 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda';
-import type { JwtPayload } from 'jsonwebtoken';
-import * as jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../../env';
 import { createResponse } from '../../common';
 import { assertAccessible } from '../../auth/data';
 import { deleteConfig } from '../data';
-import { errorResponse, isResponsibleError, ResponsibleError } from '../../util/error';
+import { BadRequestError, errorResponse, responseAsResponsibleError } from '../../util/error';
+import { verifyPayload } from '../../util/access';
 
 export const deleteConfigHandler: APIGatewayProxyHandler = async (event) => {
 	const token = (event.headers.Authorization ?? '').replace('Bearer ', '');
@@ -13,49 +11,20 @@ export const deleteConfigHandler: APIGatewayProxyHandler = async (event) => {
 	try {
 		data = JSON.parse(event.body) as UserDeleteRequest;
 	} catch {
-		return createResponse(500, {
-			success: false,
-			error: 500,
-			errorDescription: 'Data body is malformed JSON'
-		});
+		return errorResponse(new BadRequestError('Request body is malformed JSON'));
 	}
 	if (!data || !data.id) {
-		return createResponse(500, {
-			success: false,
-			error: 500,
-			errorDescription: 'Internal error'
-		});
-	}
-	let payload: JwtPayload;
-	try {
-		payload = jwt.verify(token, JWT_SECRET) as JwtPayload;
-	} catch {
-		console.debug('malformed token');
-		return createResponse(401, {
-			success: false,
-			error: 401,
-			errorDescription: 'Unauthorized'
-		});
+		return errorResponse(new BadRequestError('Request body does not contains id field'));
 	}
 	try {
+		const payload = verifyPayload(token);
 		const id = payload.aud as string;
 		await assertAccessible(id, token, true);
 		if (data.id.toUpperCase() === 'SERVICE')
-			return errorResponse(
-				new ResponsibleError(500, 'Internal error', "Can't delete SERVICE config.")
-			);
+			return errorResponse(new BadRequestError("Can't delete SERVICE config."));
 		const res = await deleteConfig(data.id);
 		return createResponse(200, { success: true, result: res });
 	} catch (e) {
-		if (isResponsibleError(e)) {
-			return errorResponse(e as ResponsibleError);
-		}
-		console.error(e);
-		const res = {
-			success: false,
-			error: 500,
-			errorDescription: 'Internal error'
-		};
-		return createResponse(500, res);
+		responseAsResponsibleError(e);
 	}
 };
--- packages/server/src/config/handler/get.ts
+++ packages/server/src/config/handler/get.ts
@@ -1,7 +1,7 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda';
 import { createResponse } from '../../common';
 import { queryConfig } from '../data';
-import { errorResponse, isResponsibleError, ResponsibleError } from '../../util/error';
+import { responseAsResponsibleError } from '../../util/error';
 
 export const getConfigHandler: APIGatewayProxyHandler = async (event) => {
 	try {
@@ -11,14 +11,6 @@ export const getConfigHandler: APIGatewayProxyHandler = async (event) => {
 			result: configs
 		});
 	} catch (e) {
-		if (isResponsibleError(e)) {
-			return errorResponse(e as ResponsibleError);
-		}
-		return createResponse(200, {
-			success: false,
-			error: 500,
-			errorDescription: 'Internal error'
-		});
+		responseAsResponsibleError(e);
 	}
-
 };
