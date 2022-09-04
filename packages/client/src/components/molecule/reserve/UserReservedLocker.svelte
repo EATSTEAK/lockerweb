51b16335e17396104e5143d151ff4e8134c0f835
EATSTEAK <me@eatsteak.dev>
2022-09-05 05:33:38 +0900

    refactor(UserReservedLocker): unified props and applied claimedUntil data

--- packages/client/src/components/molecule/reserve/UserReservedLocker.svelte
+++ packages/client/src/components/molecule/reserve/UserReservedLocker.svelte
@@ -1,44 +1,46 @@
 <script lang='ts'>
-  import Skeleton from '../../atom/Skeleton.svelte';
+	import Skeleton from '../../atom/Skeleton.svelte';
 
-  export let reservedSection;
-  export let reservedNumber;
-  export let tillTime;
+	export let reservedLocker: ReservedLocker;
+
+	let buildingId: string;
+	let floor: string;
+	let section: string;
+	let lockerNum: number;
+	let claimedUntil: Date;
+	$: if (reservedLocker) {
+		const [building_, floor_, locker] = reservedLocker.lockerId.split('-');
+		buildingId = building_;
+		floor = floor_;
+		section = locker.slice(0, 1);
+		lockerNum = parseInt(locker.slice(1));
+		if (reservedLocker.claimedUntil) claimedUntil = reservedLocker.claimedUntil as Date;
+	}
+	const dateFormatter = new Intl.DateTimeFormat('ko', { timeStyle: 'short', dateStyle: 'short' });
+	$: claimedUntilDisplay = claimedUntil ? dateFormatter.format(claimedUntil) : '';
 </script>
-{#if reservedSection !== undefined}
-  <div class='wrap'>
-    <h4 class='text-4xl'>내 정보</h4>
-    <h5 class='text-xl text-blue-500 mt-3'>예약한 사물함</h5>
-    <div class='reserve-box flex'>
-      <div class='location-info'>
-        <div class='text-2xl font-extrabold align-middle'>구역</div>
-        <div class='location text-7xl font-extrabold'>{reservedSection}</div>
-      </div>
-      <div class='number text-primary-800 text-7xl font-extrabold ml-4'>{reservedNumber}</div>
-    </div>
-    <div class='till ml-3 mt-1'>·&nbsp{tillTime}</div>
-  </div>
+{#if section !== undefined}
+	<div class='w-full'>
+		<h4 class='text-4xl'>내 정보</h4>
+		<h5 class='text-xl text-blue-500 mt-3'>예약한 사물함</h5>
+		<div class='w-56 h-44 border-2 border-blue-400 rounded-2xl bg-white items-center flex p-2 gap-1'>
+			<div class='flex flex-col w-2/5 h-40 bg-gray-300 rounded-2xl text-center justify-center items-center'>
+				<div class='text-2xl font-extrabold align-middle'>구역</div>
+				<div class='location text-7xl font-extrabold'>{section}</div>
+			</div>
+			<div class='number text-primary-800 grow text-7xl font-extrabold text-center'>{lockerNum}</div>
+		</div>
+		{#if claimedUntil}
+			<div class='till ml-3 mt-1'>{claimedUntilDisplay} 까지</div>
+		{/if}
+	</div>
 {:else}
-  <div class='wrap'>
-    <Skeleton class='w-52 h-10 mt-9 bg-gray-300 rounded-lg'></Skeleton>
-    <div class='flex flex-row mt-6'>
-      <Skeleton class='w-16 h-32 bg-gray-300 rounded-lg mr-2'></Skeleton>
-      <Skeleton class='w-36 h-32 bg-gray-300 rounded-lg'></Skeleton>
-    </div>
-    <Skeleton class='w-24 h-6 mt-2 bg-gray-300 rounded-lg'></Skeleton>
-  </div>
+	<div class='w-full'>
+		<Skeleton class='w-52 h-10 mt-9 bg-gray-300 rounded-lg'></Skeleton>
+		<div class='flex flex-row mt-6'>
+			<Skeleton class='w-16 h-32 bg-gray-300 rounded-lg mr-2'></Skeleton>
+			<Skeleton class='w-36 h-32 bg-gray-300 rounded-lg'></Skeleton>
+		</div>
+		<Skeleton class='w-24 h-6 mt-2 bg-gray-300 rounded-lg'></Skeleton>
+	</div>
 {/if}
-
-<style>
-  .reserve-box {
-    @apply w-56 h-44 border-2 border-blue-400 rounded-2xl bg-white items-center;
-  }
-
-  .location-info {
-    @apply flex flex-col w-2/5 h-40 bg-gray-300 rounded-2xl ml-2 text-center justify-center items-center;
-  }
-
-  .wrap {
-    @apply w-full;
-  }
-</style>
