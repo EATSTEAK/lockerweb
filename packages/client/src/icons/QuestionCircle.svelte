d8b917778e001833635e57ef936ccd4e871de4b2
EATSTEAK <me@eatsteak.dev>
2022-09-05 00:10:40 +0900

    feat(icons): add `QuestionCircle` icon

---
+++ packages/client/src/icons/QuestionCircle.svelte
@@ -0,0 +1,41 @@
+<script lang='ts'>
+	let className = undefined;
+	export { className as class };
+	export let id = undefined;
+	export let tabindex = undefined;
+	export let focusable = false;
+	export let title = undefined;
+	export let style = undefined;
+
+	$: ariaLabel = $$props['aria-label'];
+	$: ariaLabelledBy = $$props['aria-labelledby'];
+	$: labelled = ariaLabel || ariaLabelledBy || title;
+	$: attributes = {
+		'aria-label': ariaLabel,
+		'aria-labelledby': ariaLabelledBy,
+		'aria-hidden': labelled ? undefined : true,
+		role: labelled ? 'img' : undefined,
+		focusable: tabindex === '0' ? true : focusable,
+		tabindex
+	};
+</script>
+<svg on:click
+		 on:focus
+		 on:mouseover
+		 on:mouseenter
+		 on:mouseleave
+		 on:keyup
+		 on:keydown
+		 width='24'
+		 height='24'
+		 fill='none'
+		 viewBox='0 0 24 24'
+		 xmlns='http://www.w3.org/2000/svg'
+		 class={className}
+		 {style}
+		 {id}
+		 {...attributes}>
+	<path
+		d='M12 2c5.523 0 10 4.478 10 10s-4.477 10-10 10S2 17.522 2 12 6.477 2 12 2Zm0 13.5a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm0-8.75A2.75 2.75 0 0 0 9.25 9.5a.75.75 0 0 0 1.493.102l.007-.102a1.25 1.25 0 1 1 2.5 0c0 .539-.135.805-.645 1.332l-.135.138c-.878.878-1.22 1.447-1.22 2.53a.75.75 0 0 0 1.5 0c0-.539.135-.805.645-1.332l.135-.138c.878-.878 1.22-1.447 1.22-2.53A2.75 2.75 0 0 0 12 6.75Z'
+		fill='currentColor' />
+</svg>
