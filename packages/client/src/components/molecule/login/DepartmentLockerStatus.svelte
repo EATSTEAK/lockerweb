1ff83e5c63eca3bd6a5ff798177e7a8ee806bd1b
EATSTEAK <me@eatsteak.dev>
2022-09-04 20:24:24 +0900

    feat: apply skeleton in `LockerStatus`

--- packages/client/src/components/molecule/login/DepartmentLockerStatus.svelte
+++ packages/client/src/components/molecule/login/DepartmentLockerStatus.svelte
@@ -1,29 +1,27 @@
 <script lang='ts'>
-    import FloorStatus from '../../atom/FloorStatus.svelte';
+	import FloorStatus from '../../atom/FloorStatus.svelte';
+	import Skeleton from '../../atom/Skeleton.svelte';
 
-    export let departmentStatus;
+	export let departmentStatus;
 </script>
 {#if departmentStatus}
+	<h3><span class='text-primary-800'>{departmentStatus.departmentName}</span> 층별 예약 현황</h3>
+	<p>{departmentStatus.departmentName} 관련 문의: {departmentStatus.contact}</p>
 
-    <h3><span class='dept-name'>{departmentStatus.departmentName}</span> 층별 예약 현황</h3>
-    <p>{departmentStatus.departmentName} 관련 문의: {departmentStatus.contact}</p>
-
-    <div class='list'>
-        {#each Object.entries(departmentStatus?.floors ?? {}) as [key, value], index(key)}
-            <FloorStatus class='my-2' floor={key} lockerLeft={value.lockerLeft}
-                         totalLocker={value.totalLocker}/>
-        {/each}
-    </div>
+	<div class='mt-5'>
+		{#each Object.entries(departmentStatus?.floors ?? {}) as [key, value], index(key)}
+			<FloorStatus class='my-2' floor={key} lockerLeft={value.lockerLeft}
+									 totalLocker={value.totalLocker} />
+		{/each}
+	</div>
 {:else}
-    로딩중
+	<div class='flex flex-col gap-1'>
+		<Skeleton class='h-12 w-96 bg-gray-300 rounded-md' />
+		<Skeleton class='h-6 w-56 bg-gray-300 rounded-md' />
+	</div>
+	<div class='mt-5'>
+		<Skeleton class='h-12 w-full bg-gray-300 rounded-2xl my-2' />
+		<Skeleton class='h-12 w-full bg-gray-300 rounded-2xl my-2' />
+		<Skeleton class='h-12 w-full bg-gray-300 rounded-2xl my-2' />
+	</div>
 {/if}
-
-<style>
-    .dept-name {
-        @apply text-primary-800;
-    }
-
-    .list {
-        @apply mt-5;
-    }
-</style>
