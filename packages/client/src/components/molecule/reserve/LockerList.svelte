5ee8315e5eee88ca6b17cfbe19d6f58744c6a181
EATSTEAK <me@eatsteak.dev>
2022-09-14 00:28:54 +0900

    chore: fix `LockerList` prettier lint

--- packages/client/src/components/molecule/reserve/LockerList.svelte
+++ packages/client/src/components/molecule/reserve/LockerList.svelte
@@ -2,9 +2,9 @@
   import { createEventDispatcher } from 'svelte';
   import LockerItem from './LockerItem.svelte';
 
-	export let selectedId: string;
-	export let lockers: { lockerId: string; disabled: boolean; reserved: boolean }[] = [];
-	export let height: number;
+  export let selectedId: string;
+  export let lockers: { lockerId: string; disabled: boolean; reserved: boolean }[] = [];
+  export let height: number;
 
   const dispatch = createEventDispatcher();
   $: if (selectedId) {
@@ -13,17 +13,15 @@
 </script>
 
 <div
-	class='grid grid-flow-col gap-4 mb-16 mx-auto p-4'
-	style={`grid-template-rows: repeat(${height ?? 5}, minmax(0, 1fr));`}
->
-	{#each lockers as { lockerId, disabled, reserved }}
-		<LockerItem
-			class='my-2'
-			id={lockerId}
-			disabled={disabled || reserved}
-			on:click={() => (selectedId = lockerId)}
-			selected={selectedId === lockerId}
-			titleOverride={reserved ? '예약됨' : disabled ? '예약 불가' : undefined}
-		/>
-	{/each}
+  class="mx-auto mb-16 grid grid-flow-col gap-4 p-4"
+  style={`grid-template-rows: repeat(${height ?? 5}, minmax(0, 1fr));`}>
+  {#each lockers as { lockerId, disabled, reserved }}
+    <LockerItem
+      class="my-2"
+      id={lockerId}
+      disabled={disabled || reserved}
+      on:click={() => (selectedId = lockerId)}
+      selected={selectedId === lockerId}
+      titleOverride={reserved ? '예약됨' : disabled ? '예약 불가' : undefined} />
+  {/each}
 </div>
