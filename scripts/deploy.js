f5668849fb992e6e0061c217672fd20771464c5d
EATSTEAK <me@eatsteak.dev>
2022-09-18 20:40:15 +0900

    fix: argument parsing in deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 const execSync = require("child_process").execSync;
 
-const arg = process.argv[2] || "";
+const arg = process.argv.slice(2).join(" ") || "";
 execSync("pnpm run -r --filter=server deploy " + arg, { stdio: [0, 1, 2] });
 execSync("pnpm run -r --filter=client deploy", { stdio: [0, 1, 2] });
