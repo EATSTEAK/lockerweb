c8e6fd2314c24e43026d81d6b768e1074eb49be5
Hyomin Koo <me@eatsteak.dev>
2023-09-17 01:25:24 +0000

    feat(client/icons): add icons - `DocumentArrowRight` - `TextGrammarCheckmark`

---
+++ packages/client/src/icons/DocumentArrowRight.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  let className = undefined;
+  export { className as class };
+  export let id = undefined;
+  export let tabindex = undefined;
+  export let focusable = false;
+  export let title = undefined;
+  export let style = undefined;
+
+  $: ariaLabel = $$props['aria-label'];
+  $: ariaLabelledBy = $$props['aria-labelledby'];
+  $: labelled = ariaLabel || ariaLabelledBy || title;
+  $: attributes = {
+    'aria-label': ariaLabel,
+    'aria-labelledby': ariaLabelledBy,
+    'aria-hidden': labelled ? undefined : true,
+    role: labelled ? 'img' : undefined,
+    focusable: tabindex === '0' ? `${true}` : `${focusable}`,
+    tabindex,
+  };
+</script>
+
+<svg
+  width="24"
+  height="24"
+  fill="none"
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+  class={className}
+  {style}
+  {id}
+  {...attributes}>
+    <path d="M12 2v6a2 2 0 0 0 2 2h6v10a2 2 0 0 1-2 2h-6.81A6.5 6.5 0 0 0 4 11.498V4a2 2 0 0 1 2-2h6Zm1.5.5V8a.5.5 0 0 0 .5.5h5.5l-6-6Zm-1.5 15a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0ZM3.5 17a.5.5 0 0 0 0 1h4.793l-1.647 1.646a.5.5 0 0 0 .708.708l2.5-2.5a.5.5 0 0 0 0-.708l-2.5-2.5a.5.5 0 0 0-.708.708L8.293 17H3.5Z" fill="currentColor"/>
+</svg>
